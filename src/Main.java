ab195d8ee1654a552847e1b0d4855a7d0863c1f5
Leo Zeng <qingchun1995@gmail.com>
2024-07-14 19:03:16 +0800

    Add Iterative Way and Sorting to find maximum price

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Initialize the array with 10 days of opening stock prices
-        float[] openingPrices = {100.5f, 102.3f, 101.0f, 99.8f, 103.4f, 104.1f, 105.0f, 106.2f, 102.5f, 1.1f, 2.2f, 15.1f};
+        float[] openingPrices = {100.5f, 102.3f, 101.0f, 99.8f, 118.4f, 104.1f, 105.0f, 106.2f, 102.5f, 12.3f};
         StockAnalysis obj = new StockAnalysis(openingPrices);
 
         System.out.println("The average price is: " + obj.calculateAveragePrice());
--- src/StockAnalysis.java
+++ src/StockAnalysis.java
@@ -22,10 +22,22 @@ public class StockAnalysis {
     // Find the maximum stock price
     // The method takes the array of stock prices as input and returns the maximum price among all the stocks
     public float findMaximumPrice() {
-        Arrays.sort(stockPrices);
-        return stockPrices[stockPricesLength - 1];
+        // Sorting
+        // Arrays.sort(stockPrices);
+        // return stockPrices[stockPricesLength - 1];
+
+        // Iterative Way
+        float maxPrice = stockPrices[0];
+        for (int i = 0; i < stockPricesLength; i++) {
+            if (stockPrices[i] > maxPrice) {
+                maxPrice = stockPrices[i];
+            }
+        }
+        return maxPrice;
     }
 
+    // Determine the occurrence count of a specific price
+    // The method takes the array of stock prices and a target price as input and returns the number of times the target price occurs in the array
     public void countOccurrences() {
     }
 }
