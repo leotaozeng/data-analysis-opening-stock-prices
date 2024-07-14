3cc4aba269ccf3110e273c90304a4ca1be3001a7
Leo Zeng <qingchun1995@gmail.com>
2024-07-14 22:14:47 +0800

    Initialize an ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        // Initialize the array with 10 days of opening stock prices
-        float[] openingPrices = {100.5f, 102.3f, 101.0f, 12.3f, 118.4f, 104.1f, 105.0f, 106.2f, 102.5f, 12.3f};
-        StockAnalysis obj = new StockAnalysis(openingPrices);
+        // Initialize an Array with 10 days of opening stock prices
+        float[] stockPricesArray = {100.5f, 102.3f, 101.0f, 12.3f, 118.4f, 104.1f, 105.0f, 106.2f, 102.5f, 12.3f};
 
+        // Initialize an ArrayList with 10 days of opening stock prices
+        ArrayList<Float> stockPricesList = new ArrayList<>(Arrays.asList(100.5f, 102.3f, 101.0f, 12.3f, 118.4f, 104.1f, 105.0f, 106.2f, 102.5f, 12.3f));
+
+        StockAnalysis obj = new StockAnalysis(stockPricesArray, stockPricesList);
+
+        // Generate a random number between 0 and 9
         Random rand = new Random();
         int randomNum = rand.nextInt(10);
 
-        System.out.println("The average price is: " + obj.calculateAveragePrice());
-        System.out.println("The maximum price is: " + obj.findMaximumPrice());
-        System.out.println("The target price " + openingPrices[randomNum] + " occurs " +  obj.countOccurrences(openingPrices[randomNum]) + " times.");
+        System.out.println("The average stock price is: " + obj.calculateAveragePrice());
+        System.out.println("The maximum stock price is: " + obj.findMaximumPrice());
+        System.out.println("The target stock price is " + stockPricesArray[randomNum] + ", which occurs " +  obj.countOccurrences(stockPricesArray[randomNum]) + " times");
+        System.out.println("the cumulative sum price is " + obj.computeCumulativeSum());
     }
 }
