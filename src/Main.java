f941de4c9b54b336127d504235f9b1164d69fc1f
Leo Zeng <qingchun1995@gmail.com>
2024-07-14 18:20:26 +0800

    Create findMaximumPrice method

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-
-
         // Initialize the array with 10 days of opening stock prices
-        float[] openingPrices = {100.5f, 102.3f, 101.0f, 99.8f, 103.4f, 104.1f, 105.0f, 106.2f, 102.5f, 101.3f};
-        StockAnalysis.calculateAveragePrice(openingPrices);
+        float[] openingPrices = {100.5f, 102.3f, 101.0f, 99.8f, 103.4f, 104.1f, 105.0f, 106.2f, 102.5f, 1.1f, 2.2f, 15.1f};
+        StockAnalysis obj = new StockAnalysis(openingPrices);
+
+        System.out.println("The average price is: " + obj.calculateAveragePrice());
+        System.out.println("The maximum price is: " + obj.findMaximumPrice());
     }
 }
--- src/StockAnalysis.java
+++ src/StockAnalysis.java
@@ -1,11 +1,31 @@
+import java.util.Arrays;
+
 public class StockAnalysis {
+    private final float[] stockPrices;
+    private final int stockPricesLength;
+
+    public StockAnalysis(float[] stockPrices) {
+        this.stockPrices = stockPrices;
+        this.stockPricesLength = stockPrices.length;
+    }
+
     // Calculate the average stock price
-    public static void calculateAveragePrice(float[] stockPrices) {
+    // The method takes the array of stock prices as input and returns the average price of the stocks
+    public float calculateAveragePrice() {
         float sum = 0;
         for (float stockPrice : stockPrices) {
             sum += stockPrice;
         }
-        float averagePrice = sum / stockPrices.length;
-        System.out.println("The average price is: " + averagePrice);
+        return sum / stockPricesLength;
+    }
+
+    // Find the maximum stock price
+    // The method takes the array of stock prices as input and returns the maximum price among all the stocks
+    public float findMaximumPrice() {
+        Arrays.sort(stockPrices);
+        return stockPrices[stockPricesLength - 1];
+    }
+
+    public void countOccurrences() {
     }
 }
